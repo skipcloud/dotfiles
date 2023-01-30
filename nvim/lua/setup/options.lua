87fd0d136ccc8202b9de6957de351f159a6b7dbc
Skip Gibson <skip.gibson@hey.com>
2023-01-30 09:33:31 +0000

    nvim/options: fix grepprg

--- nvim/lua/setup/options.lua
+++ nvim/lua/setup/options.lua
@@ -5,7 +5,7 @@ local bo = vim.bo
 vim.g.python3_host_prog="/home/skip/.pyenv/shims/python"
 
 -- Global options
-o.statusline = 3
+o.laststatus = 3
 o.cmdheight = 2
 o.updatetime = 300
 o.path = o.path .. ",./**"
@@ -16,7 +16,7 @@ o.ignorecase = true
 o.smartcase = true
 o.mouse = "a"
 o.dictionary = "/usr/share/dict/words"
-o.grepprg = "rg --vimgrep --noheading --smart-case --hidden --glob !vendor --glob !node_modules"
+o.grepprg = "rg --vimgrep --no-heading --smart-case --hidden --glob !vendor --glob !node_modules"
 o.ttimeoutlen = 20
 o.directory = "/tmp//," .. o.directory
 o.autowrite = true
