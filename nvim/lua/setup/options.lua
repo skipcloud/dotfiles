d75f83f5e1a309edca55a0025013011040659869
Skip Gibson <skip.gibson@hey.com>
2023-04-22 17:38:32 +0100

    nvim/opts: use convenient list/maps opt and ignore .git when grepping

--- nvim/lua/setup/options.lua
+++ nvim/lua/setup/options.lua
@@ -1,8 +1,9 @@
 local o = vim.o
+local opt = vim.opt -- convenient interface for interacting with lists/maps
 local wo = vim.wo
 local bo = vim.bo
 
-vim.g.python3_host_prog="/home/skip/.pyenv/shims/python"
+vim.g.python3_host_prog = "/home/skip/.pyenv/shims/python"
 
 -- Global options
 o.laststatus = 3
@@ -16,22 +17,21 @@ o.ignorecase = true
 o.smartcase = true
 o.mouse = "a"
 o.dictionary = "/usr/share/dict/words"
-o.grepprg = "rg --vimgrep --no-heading --smart-case --hidden --glob !vendor --glob !node_modules"
+o.grepprg = "rg --vimgrep --no-heading --smart-case --hidden --glob !.git --glob !vendor --glob !node_modules"
 o.ttimeoutlen = 20
-o.directory = "/tmp//," .. o.directory
 o.autowrite = true
-o.tags = "./.git/tags," .. o.tags
 o.completeopt = "menu"
+opt.directory:prepend { "/tmp//" }
+opt.tags:prepend { "./.git/tags" }
 
 -- Window options
 wo.number = true
 wo.linebreak = true
 wo.wrap = false
-wo.concealcursor="n" -- mode in which concealed text is revealed
+wo.concealcursor = "n" -- mode in which concealed text is revealed
 
 -- Buffer options
 bo.softtabstop = 2
 bo.expandtab = true
 bo.shiftwidth = 2
 bo.undofile = true
-
